bebe25eebb43d7107e7714ef2b24803f8fcb6d85
indiagayapetshop-byte <indiagaya.petshop@gmail.com>
2026-03-15 00:29:53 +0100

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,35 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA5OUS8Cd8BdxyzTiLSgh-BtYYwMkAKjf0k",
+  authDomain: "indylife-59c2f.firebaseapp.com",
+  projectId: "indylife-59c2f",
+  storageBucket: "indylife-59c2f.appspot.com",
+  messagingSenderId: "834966946217",
+  appId: "1:834966946217:web:34d47225d6820857fa44b8"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title =
+    payload.notification?.title ||
+    payload.data?.title ||
+    "IndyLife";
+
+  const body =
+    payload.notification?.body ||
+    payload.data?.body ||
+    "";
+
+  const icon =
+    payload.notification?.icon ||
+    "/icon-192.png";
+
+  self.registration.showNotification(title, {
+    body,
+    icon,
+    data: payload.data || {}
+  });
+});
